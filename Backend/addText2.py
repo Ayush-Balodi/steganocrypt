a6967c835bccefba551dbfe47a89e77cb40f0a54
Ayush Balodi <87846532+Ayush-Balodi@users.noreply.github.com>
2023-06-09 12:58:01 +0530

    Updated the AES encryption

--- Backend/addText2.py
+++ Backend/addText2.py
@@ -1,65 +1,32 @@
-import base64
 import hashlib
 from Crypto.Cipher import AES
-from PIL import Image
+from Crypto.Util.Padding import pad
+import base64
 
-# making the image object
-original_image = Image.open('origin.png')
+def encrypt_message(message, password):
+    # Generate salt
+    salt = b'\x28\xAB\xBC\xCD\xDE\xEF\x00\x33'
 
-# loading the pixel values of the original image, each one is a pixel
-original_pixelMap = original_image.load()
+    # Derive key from password and salt using MD5
+    key = hashlib.md5(password.encode('utf-8') + salt).digest()
 
-encrypt_image = Image.new(original_image.mode, original_image.size)
-encrypt_pixelMap = encrypt_image.load()
+    # Create AES cipher object with CBC mode
+    cipher = AES.new(key, AES.MODE_CBC)
 
-# Reading the message from the user
-msg = input("Enter the message: ")
-msg_index = 0
+    # Pad the message
+    padded_message = pad(message.encode('utf-8'), AES.block_size)
 
+    # Encrypt the padded message
+    encrypted_message = cipher.encrypt(padded_message)
+
+    # Concatenate the IV and encrypted message
+    iv_and_message = cipher.iv + encrypted_message
+
+    # Return the base64-encoded ciphertext
+    return base64.b64encode(iv_and_message).decode('utf-8')
+
+message = input("Enter the message: ")
 password = "Password"
-salt = b'\x28\xAB\xBC\xCD\xDE\xEF\x00\x33'
-key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000, dklen=16)
-m = hashlib.md5(key)
-key = m.digest()[:16]  # 16-byte key for AES
 
-# Generate a 16-byte initialization vector (IV)
-iv = hashlib.md5(salt).digest()[:16]
-
-crypter = AES.new(key, AES.MODE_CBC, iv)
-
-print("The message is:", msg)
-padding_length = 16 - len(msg) % 16
-padding = bytes([padding_length] * padding_length)
-msg = msg.encode('utf-8')
-msg += padding
-ciphertext = crypter.encrypt(msg)
-encode_string = base64.b32encode(ciphertext)
-print("The encoded string is:", encode_string.decode('utf-8'))
-msg = encode_string.decode('utf-8')
-msg_len = len(msg)
-
-for row in range(original_image.size[0]):
-    for col in range(original_image.size[1]):
-
-        pixel = original_pixelMap[row, col]
-        r = pixel[0]  # Red value
-        g = pixel[1]  # Green value
-        b = pixel[2]  # Blue value
-
-        if row == 0 and col == 0:
-            ascii_val = msg_len
-            encrypt_pixelMap[row, col] = (ascii_val, g, b)
-        elif msg_index < msg_len:
-            c = msg[msg_index]
-            ascii_val = ord(c)
-            encrypt_pixelMap[row, col] = (ascii_val, g, b)
-        else:
-            encrypt_pixelMap[row, col] = (r, g, b)
-        msg_index += 1
-
-original_image.close()
-
-encrypt_image.show()
-
-encrypt_image.save("encrypted_image.png")
-encrypt_image.close()
+encrypted_message = encrypt_message(message, password)
+print("Encrypted message:", encrypted_message)
--- Backend/extractText2.py
+++ Backend/extractText2.py
@@ -1,38 +1,32 @@
-import base64
-import hashlib 
+import hashlib
 from Crypto.Cipher import AES
-from PIL import Image
+from Crypto.Util.Padding import unpad
+import base64
+
+def decrypt_message(ciphertext, password):
+    salt = b'\x28\xAB\xBC\xCD\xDE\xEF\x00\x33'
+    key = hashlib.md5(password.encode('utf-8') + salt).digest()
+
+    # Extract the IV and encrypted message from the base64-encoded ciphertext
+    iv_and_message = base64.b64decode(ciphertext)
+
+    # Split the IV and encrypted message
+    iv = iv_and_message[:AES.block_size]
+    encrypted_message = iv_and_message[AES.block_size:]
+
+    # Create AES cipher object with CBC mode
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+
+    # Decrypt the encrypted message
+    decrypted_message = cipher.decrypt(encrypted_message)
+
+    # Unpad the decrypted message
+    unpadded_message = unpad(decrypted_message, AES.block_size)
+
+    return unpadded_message.decode('utf-8')
+
+ciphertext = input("Enter the ciphertext: ")
 
 password="Password"
-salt = b'\x28\xAB\xBC\xCD\xDE\xEF\x00\x33'
-key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000, dklen=16)
-key = key[:16]
-iv = salt
-crypter = AES.new(key, AES.MODE_CBC, iv)
-
-encrypt_image=Image.open('encrypted_image.png')
-
-encrypt_pixelMap=encrypt_image.load()
-
-msg=""
-msg_index=0
-for row in range(encrypt_image.size[0]):
-    for col in range(encrypt_image.size[1]):
-
-        list=encrypt_pixelMap[row, col]
-        r=list[0]
-
-        if row==0 and col==0:
-            msg_len=r
-        elif msg_len>msg_index:
-            msg = msg+chr(r)
-            msg_index = msg_index+1
-    
-encrypt_image.close()
-
-print("The encrypted message is : ", msg)
-
-encypted_string = msg
-encypted_string = base64.b32decode(encypted_string)
-decrypted_string = crypter.decrypt(encypted_string)
-print("The original message was : ",decrypted_string.decode('utf-8'))
+decrypted_message = decrypt_message(ciphertext, password)
+print("Decrypted message:", decrypted_message)
